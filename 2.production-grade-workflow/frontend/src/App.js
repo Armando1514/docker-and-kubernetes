8bb852b97e84df6cc08aa4d9168977b4e68d3b85
armando.ferrara <armando.ferrara@dormakaba.com>
2023-03-23 18:56:02 +0100

    pull request to trigger gitaction for production grade worflow

--- 2.production-grade-workflow/frontend/src/App.js
+++ 2.production-grade-workflow/frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, CHANGED
+          Learn React, CHANGED - PULL REQUEST
         </a>
       </header>
     </div>
